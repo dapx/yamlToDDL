296d22a2751e4f6a444a5cbc47c81f55a03e9a2d
Daniel André <dap1995@gmail.com>
2016-06-23 21:47:45 -0300

    Adicionado alter table e comparacao de tabelas

--- src/main/java/model/Table.java
+++ src/main/java/model/Table.java
@@ -2,6 +2,9 @@ package model;
 
 import java.util.List;
 
+import exception.MismatchedTypeException;
+import exception.LengthDecreaseException;
+
 public class Table {
 	private String name;
 	private List<Column> columns;
@@ -28,10 +31,10 @@ public class Table {
 
 	@Override
 	public String toString() {
-		return "Table [name=" + name + "]";
+		return "Table [name=" + name + "] Columns " + columns.toString();
 	}
 	
-	public String toTable(){
+	public String createTable(){
 		StringBuilder sb = new StringBuilder();
 		sb.append("CREATE TABLE ").append(name).append(" ( \n");
 		for (Column column : columns){
@@ -41,4 +44,40 @@ public class Table {
 		sb.append(");");
 		return sb.toString();
 	}
-}
+	
+	public String alterTable(){
+		StringBuilder sb = new StringBuilder();
+		for (Column column : columns){
+		sb.append("ALTER TABLE ").append(name).append(" add ").append(column.getName()).append(" ").append(column.getType()).append("(").append(column.getLength()).append("); \n");
+		}
+		return sb.toString();
+	}
+	
+	//VOU REMOVER ESSE METODO HORRIVEL ||| FIQUE TRANQUILO
+	public String compareWith(Table table){
+		StringBuilder sb = new StringBuilder();
+		//this.table é o META-DADOS ORIGINAL
+		//table é o META-DADOS obtido de uma tabela do banco
+		for (Column column : this.columns){
+			for (Column column2 : table.columns){
+				if (column.getName().equals(column2.getName())){
+					column.setModify(true);
+					if (column.getType().equals(column2.getType())){
+						//Na linha abaixo ele verifica se a coluna de mesmo nome e tipo da tabela original(atualizada) é menor que a que está no banco
+						if (column.getLength() < column2.getLength()){
+							throw new LengthDecreaseException("unable to decrease size of column: " + column.getName());
+						} else if (column.getLength() > column2.getLength()) {
+							sb.append("ALTER TABLE ").append(this.name).append(" modify ").append(column.getName()).append(" ").append(column.getType()).append("(").append(column.getLength()).append("); \n");
+						}
+					} else {
+						throw new MismatchedTypeException("unable to convert type of column: " + column.getName());
+					}
+				}
+			}
+			if (!column.isModify()) {
+				sb.append("ALTER TABLE ").append(this.name).append(" add ").append(column.getName()).append(" ").append(column.getType()).append("(").append(column.getLength()).append("); \n");
+			}
+		}
+		return sb.toString();
+	}
+} 
