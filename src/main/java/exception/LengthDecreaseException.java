765f7dc2afd512af24aa3dca6087cad3cccaf4d9
Daniel André <dap1995@gmail.com>
2016-06-23 21:48:58 -0300

    Criadas duas exceções para a comparação de tabelas

---
+++ src/main/java/exception/LengthDecreaseException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class LengthDecreaseException extends RuntimeException {
+	public LengthDecreaseException(String msg) {
+		super(msg);
+	}
+}
---
+++ src/main/java/exception/MismatchedTypeException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class MismatchedTypeException extends RuntimeException {
+ public MismatchedTypeException(String msg) {
+	super(msg);
+ }
+}
